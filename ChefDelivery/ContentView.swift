13a84025b9e04c6ea4678f89ff6d31dcdcf0807a
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-08 17:31:50 -0300

    Declaring the first order in SwiftUI

--- ChefDelivery/ContentView.swift
+++ ChefDelivery/ContentView.swift
@@ -10,15 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Text("Hello, world!")
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewLayout(.sizeThatFits)
+    }
 }
+
+
+//#Preview {
+//    ContentView()
+//        .previewLayout(.sizeThatFits)
+//}
