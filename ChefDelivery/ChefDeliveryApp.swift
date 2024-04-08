cd77ffbb9278d7de927a2537a3c2c1756ce08bc8
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-08 12:03:54 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Matheus Santos on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Matheus Santos on 08/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
