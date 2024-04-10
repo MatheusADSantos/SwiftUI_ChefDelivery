427baff5f4b654aad74ebb7e64a038faf17701d6
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-10 17:33:55 -0300

    Implementing StoreDetailView

--- ChefDelivery/StoreView/StoreDetailView.swift
+++ ChefDelivery/StoreView/StoreDetailView.swift
@@ -12,12 +12,32 @@ struct StoreDetailView: View {
     let store: StoreType
     
     var body: some View {
-        Text("Tela - \(store.name)")
+        VStack(alignment: .leading) {
+            Image(store.headerImage).resizable().scaledToFit()
+            
+            HStack {
+                Text(store.name).font(.title).bold()
+                Spacer()
+                Image(store.logoImage)
+            }.padding(.vertical, 8).padding(.horizontal)
+            
+            HStack {
+                Text(store.location)
+                Spacer()
+                ForEach(1...store.stars, id: \.self) { _ in
+                    Image(systemName: "star.fill").foregroundColor(.yellow).font(.caption)
+                }
+            }.padding(.vertical, 8).padding(.horizontal)
+            
+            Text("Produtos").font(.title2).bold().padding()
+        }
+        .navigationTitle(store.name)
+        .navigationBarTitleDisplayMode(.automatic)
     }
 }
 
 struct StoreDetailView_Preview: PreviewProvider {
     static var previews : some View {
-        StoreDetailView(store: storesMock[0])
+        StoreDetailView(store: storesMock[1])
     }
 }
