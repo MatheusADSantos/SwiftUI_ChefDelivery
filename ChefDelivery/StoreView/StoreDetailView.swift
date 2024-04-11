a91362eda5984162306e9517d1fe71671506071a
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-11 11:13:08 -0300

    ToolBar > NavigationBar: Implemented custom navigationBar through ToolBar PS: All navigationBar is a ToolBar ...

--- ChefDelivery/StoreView/StoreDetailView.swift
+++ ChefDelivery/StoreView/StoreDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct StoreDetailView: View {
     
     let store: StoreType
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         ScrollView(showsIndicators: false) {
@@ -47,7 +48,21 @@ struct StoreDetailView: View {
                 }
             }
             .navigationTitle(store.name)
-        .navigationBarTitleDisplayMode(.automatic)
+            .navigationBarTitleDisplayMode(.automatic)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        HStack(spacing: 4) {
+                            Image(systemName: "cart")
+                            Text("Lojas")
+                        }.foregroundColor(Color("ColorRed"))
+                    }
+
+                }
+            }
         }
     }
 }
