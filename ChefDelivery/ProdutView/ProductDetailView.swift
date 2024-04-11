0d6dac2cb4d39baf278487172a770312d575f01e
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-11 12:11:53 -0300

    Implemented ProductDetailView layout

--- ChefDelivery/ProdutView/ProductDetailView.swift
+++ ChefDelivery/ProdutView/ProductDetailView.swift
@@ -12,7 +12,45 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
-        Text(product.name)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .shadow(radius: 20)
+                
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                Text(product.description)
+                    .padding(.horizontal)
+                Text(product.formatePrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+            Spacer()
+            
+            Button(action: {
+                print("Button foi presionado")
+            }, label: {
+                HStack {
+                    Image(systemName: "cart")
+                    Text("Adicionar ao carinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical, 16)
+                .font(.title3)
+                .bold()
+                .background(Color("ColorRed"))
+                .foregroundColor(.white)
+                .cornerRadius(32)
+                .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
+            })
+            
+        }.padding(6)
     }
 }
 
