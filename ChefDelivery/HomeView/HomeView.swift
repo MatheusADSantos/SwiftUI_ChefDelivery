512ade1588dc4ea06b0ad6c2a856e44c78c3fbda
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-15 18:28:05 -0300

    DragGesture: Moving image

--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     
     @State var isAnimating = false
+    @State var imageOffset : CGSize = .zero
     
     var body: some View {
         GeometryReader { geometry in
@@ -37,13 +38,14 @@ struct HomeView: View {
                     .scaledToFit()
                     .shadow(radius: 60)
                     .padding(32)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .gesture(
                         DragGesture()
                             .onChanged({ gesture in
-                                print(gesture.translation)
+                                imageOffset = gesture.translation
                             })
                             .onEnded({ _ in
-                                print("A interação acabou")
+                                imageOffset = .zero
                             })
                     )
                 
