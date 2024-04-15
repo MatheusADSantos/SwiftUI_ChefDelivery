69dc1ab0cae41fc66ebec499121c6f76607f00a5
Matheus Santos <matheus.santos@ftdata.com.br>
2024-04-15 12:46:24 -0300

    adding blur animation to background

--- ChefDelivery/HomeView/HomeView.swift
+++ ChefDelivery/HomeView/HomeView.swift
@@ -9,32 +9,54 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State var isAnimation = false
+    @State var isAnimating = false
     
     var body: some View {
-        VStack {
-            Text("Chef Delivey")
-                .font(.system(size: 40))
-                .fontWeight(.heavy)
-                .foregroundColor(Color("ColorRed"))
-                .opacity(isAnimation ? 1 : 0)
-                .offset(y: isAnimation ? 0 : -40)
-            
-            Text("Peça suas comidas no conforto de sua casa.")
-                .font(.title2)
-                .padding()
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black.opacity(0.7))
-                .opacity(isAnimation ? 1 : 0)
-                .offset(y: isAnimation ? 0 : -40)
-            
-            Spacer()
-        }
-        
-        
-        .onAppear {
-            withAnimation(.easeInOut(duration: 1.5)) {
-                isAnimation = true
+        GeometryReader { geometry in
+            ZStack {
+                
+                Circle()
+                    .foregroundColor(Color("ColorRed"))
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(x: isAnimating ? 50 : -50, y: isAnimating ? 100 : -100)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                Circle()
+                    .foregroundColor(Color("ColorRedDark"))
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(
+                        x: isAnimating ? geometry.size.width - 50 : geometry.size.width + 50,
+                        y: isAnimating ? geometry.size.height - 50 : geometry.size.height + 50
+                    )
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+                
+                VStack {
+                    Text("Chef Delivey")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("ColorRed"))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Text("Peça suas comidas no conforto de sua casa.")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+                    
+                    Spacer()
+                }
+                
+                
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        isAnimating = true
+                    }
+                }
             }
         }
     }
